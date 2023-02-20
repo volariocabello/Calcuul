e1f6535c2f3a270ec5d8a1712d031af2bb40aa08
Volario <volariocabello@proton.me>
2023-02-20 02:15:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const screen = document.querySelector(".screen");
+const buttons = document.querySelectorAll(".btn");
+const clear = document.querySelector(".btn-clear");
+const equal = document.querySelector(".btn-equal");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function(e) {
+        let value = e.target.dataset.num;
+        screen.value += value;
+    })
+});
+
+equal.addEventListener("click", function(e) {
+    if (screen.value === '') {
+        screen.value = "";
+    } else {
+        let answer = eval(screen.value)
+        screen.value = answer;
+    }
+});
+
+clear.addEventListener("click", function(e) {
+    screen.value = "";
+});
